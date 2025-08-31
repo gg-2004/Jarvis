0166f6d2b2958424aa89f99b22b20a9d257f20d8
gg-2004 <138923116+gg-2004@users.noreply.github.com>
2025-08-31 12:28:20 +0530

    jarvis python

--- engine/db.py
+++ engine/db.py
@@ -6,9 +6,9 @@ cursor = con.cursor()
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100),path VARCHAR(1000))"
 cursor.execute(query)
 
-#query = "INSERT INTO sys_command VALUES(null,'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
-#cursor.execute(query)
-#con.commit()
+query = "INSERT INTO sys_command VALUES(null,'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
+cursor.execute(query)
+con.commit()
 
 
 query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
